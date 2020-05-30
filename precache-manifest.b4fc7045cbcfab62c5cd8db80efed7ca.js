f0c663966a0429228151eb335655cca35b0694d7
Laura Markovičová <itsmelararogers@MacBook-Air-uzivatela-Laura.local>
2020-05-30 18:02:27 +0200

    Updates

---
+++ precache-manifest.b4fc7045cbcfab62c5cd8db80efed7ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b618b3349ffd8b4b41466b64e16faece",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "23a181a19bb5798dc0ed",
+    "url": "/monsters-rolodex/static/css/main.2460efd6.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a181a19bb5798dc0ed",
+    "url": "/monsters-rolodex/static/js/main.64928ab3.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
